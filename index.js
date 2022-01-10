e40a5cb4b0400be74b662ebd2dab1ffed32c0715
Debbie Hall <debleehall15@gmail.com>
2022-01-09 22:27:01 -0500

    wrile file function

--- index.js
+++ index.js
@@ -84,7 +84,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(error);
+        }
+
+        console.log("Succes! Your README file has been created!")
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
